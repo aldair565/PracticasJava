590269b174769f5e3fa04a54e23e9c0c717033a5
aldair565 <96149830+aldair565@users.noreply.github.com>
2022-02-02 17:42:20 -0600

    Added Function exercises for Java

---
+++ Function/src/function/NameLength.java
@@ -0,0 +1,30 @@
+package function;
+import java.util.Scanner;
+
+public class NameLength {
+
+	public static void main(String[] args) {
+		
+		Scanner console = new Scanner(System.in);
+        String name;
+        String lname;
+        
+        System.out.print("Enter your name: ");
+        name = console.nextLine();
+        System.out.print("Enter your last name: ");
+        lname = console.nextLine();
+        
+        console.close();
+        
+        int n = name.length();
+        
+        for (int i=0; i<=n-1;i++) {
+        	System.out.println(name.charAt(i));
+        }
+        
+        System.out.println(name + " " +lname);
+        System.out.println(n);
+        
+        
+	}
+}

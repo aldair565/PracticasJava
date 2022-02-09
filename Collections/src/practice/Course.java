26968528b09b6e546d53c6dadc260f664dff4829
aldair565 <96149830+aldair565@users.noreply.github.com>
2022-02-09 09:58:25 -0600

    added Collections exercises

---
+++ Collections/src/practice/Course.java
@@ -0,0 +1,25 @@
+package practice;
+
+public class Course
+{
+
+    String name;
+
+    String id;
+
+    int credits;
+
+    //constructor
+    public Course( String name, String id, int credits )
+    {
+        this.name = name;
+        this.id = id;
+        this.credits = credits;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Course{" + "name='" + name + '\'' + ", id='" + id + '\'' + ", credits=" + credits + '}';
+    }
+}
